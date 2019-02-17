13dfda12c28bd8d6146790d5a6f5cebc3ff879fc
Derek Andrews <gradualgames@gmail.com>
2019-02-17 13:25:29 -0500

    Adding a commented out attempt to load and plot each color from ll.pal as an individual pixel. They come out black. Also adding the plotting of a single pixel inside our scaled section. I expected this to draw a big fat red pixel that one would expect from a 320x240 mode, but the pixel is tiny as though the scale is not being taken into account at all.

--- main.lua
+++ main.lua
@@ -4,6 +4,8 @@ function love.load()
     screenWidth, screenHeight = 320, 240
     graphicsWidth, graphicsHeight = love.graphics.getDimensions()
     scaleWidth, scaleHeight = graphicsWidth/screenWidth, graphicsHeight/screenHeight
+
+    --paletteData, size = love.filesystem.read("ll.pal", 768)
 end
 
 function love.update(dt)
@@ -12,6 +14,28 @@ end
 function love.draw()
     love.graphics.push()
     love.graphics.scale(scaleWidth,scaleHeight)
+
+    --Insize of our pushed scale, I would have expected this to draw a big
+    --fat pixel. Instead it is a tiny pixel of the density of the current
+    --screen.
+    love.graphics.setColor(255,0,0)
+    love.graphics.points(100,100)
+    
+    --The following code attempts to plot each color from
+    --ll.pal on the screen as a pixel. It does not work, everything
+    --comes up black.
+    --[[
+    local i = 1
+    for y=1,16 do
+        for x = 1,16 do
+            local r,g,b = string.byte(paletteData, i),
+                          string.byte(paletteData, i+1),
+                          string.byte(paletteData, i+2)
+            love.graphics.setColor(r,g,b)
+            love.graphics.points(x+100, y+100)
+        end
+    end
+    --]]
 
     love.graphics.print("Hello, World!", 110, 120)
     love.graphics.setColor(255, 255, 255)
