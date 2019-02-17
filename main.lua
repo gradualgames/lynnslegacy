082053c2ca88c07c0d072ccc8a64decd640a2578
Derek Andrews <gradualgames@gmail.com>
2019-02-17 14:48:05 -0500

    As of this commit, we have successfully loaded the Lynn's Legacy palette and plotted it on the screen. The next step will be loading some sprite data and plotting that on the screen. Sprite data is likely to consist of indices into this palette, so we will have to research how to use a paletted graphics mode if at all possible.

--- main.lua
+++ main.lua
@@ -7,6 +7,8 @@ function love.load()
     screenWidth,screenHeight = love.graphics.getDimensions()
     canvasWidth,canvasHeight = canvas:getDimensions()
     scale = math.min(screenWidth/canvasWidth , screenHeight/canvasHeight)
+
+    paletteData, size = love.filesystem.read("ll.pal", 768)
 end
 
 function love.update(dt)
@@ -14,11 +16,30 @@ end
 
 function love.draw()
     love.graphics.setCanvas(canvas)
+    love.graphics.clear()
 
     -- Render stuff in the game here
     love.graphics.setLineStyle("rough")
     love.graphics.rectangle("line",50,50,100,100)
     love.graphics.points(110,110)
+
+    --The following code attempts to plot each color from
+    --ll.pal on the screen as a pixel. It does not work, everything
+    --comes up black.
+    --[
+    print("****")
+    local i = 1
+    for y=1,16 do
+        for x = 1,16 do
+            local r,g,b = string.byte(paletteData, i),
+                          string.byte(paletteData, i+1),
+                          string.byte(paletteData, i+2)
+            print("red: "..r.." green: "..g.." blue: " ..b)
+            love.graphics.setColor(r/255,g/255,b/255)
+            love.graphics.points(x*2+100, y*2+100)
+            i = i + 3
+        end
+    end
 
     love.graphics.setCanvas()
 
@@ -27,45 +48,6 @@ function love.draw()
     love.graphics.scale(scale,scale) -- Scale
     love.graphics.draw(canvas) -- Draw the canvas
     love.graphics.pop() -- pop transformation state
-
-    --love.graphics.push()
-    --love.graphics.scale(scaleWidth,scaleHeight)
-
-    --Insize of our pushed scale, I would have expected this to draw a big
-    --fat pixel. Instead it is a tiny pixel of the density of the current
-    --screen.
-
-    -- love.graphics.setCanvas(canvas)
-
-    -- love.graphics.setColor(255,0,0)
-    -- love.graphics.points(100,100,101,100,102,100)
-
-    -- love.graphics.setCanvas()
-
-    -- love.graphics.setBlendMode("alpha", "premultiplied")
-    -- love.graphics.draw(canvas)
-
-    --The following code attempts to plot each color from
-    --ll.pal on the screen as a pixel. It does not work, everything
-    --comes up black.
-    --[[
-    local i = 1
-    for y=1,16 do
-        for x = 1,16 do
-            local r,g,b = string.byte(paletteData, i),
-                          string.byte(paletteData, i+1),
-                          string.byte(paletteData, i+2)
-            love.graphics.setColor(r,g,b)
-            love.graphics.points(x+100, y+100)
-        end
-    end
-    --]]
-
-    --love.graphics.print("Hello, World!", 110, 120)
-    --love.graphics.setColor(255, 255, 255)
-    --love.graphics.circle("fill", 300, 300, 50, 100) -- Draw white circle with 100 segments.
-
-    --love.graphics.pop()
 end
 
 function love.keypressed(key, scancode, isrepeat)
