f1ac903991f2adc3f100eaf68f229172ab6cb0cb
Derek Andrews <gradualgames@gmail.com>
2019-03-10 16:02:27 -0400

    Clarify the todo about using the low byte of the tile index since we know now the original source code does do this.

--- game/update.lua
+++ game/update.lua
@@ -8,10 +8,9 @@ function updateRoom(room, spriteSheet, spriteBatch)
         for y = 0, room.y - 1 do
             for x = 0, room.x - 1 do
 
-                --TODO: We are only considering the low byte of the tile index.
-                --This was a wild guess that the upper byte contains flags since we were
-                --getting some invalid tiles previously, and the guess was correct. So
-                --the question is: What is the upper byte for and how is it used?
+                --We consider only the low byte here for the tile index. This is
+                --what the original source code does, as well.
+                --TODO: What is the upper byte for and how is it used?
                 local tileIndex = bit.band(room.layout[layer][room.x * y + x + 1], 0xff)
 
                 local quad = love.graphics.newQuad(
