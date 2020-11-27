798b40f269f7b7de355e233858809a004ee85387
Derek Andrews <gradualgames@gmail.com>
2020-11-27 13:13:15 -0500

    Implementing __stop_sound.

--- game/object_sound.lua
+++ game/object_sound.lua
@@ -1,3 +1,20 @@
+-- Function __stop_sound ( this As _char_type Ptr ) As Integer
+function __stop_sound(this)
+--
+--   #IfDef ll_audio
+--     BASS_ChannelStop( this->playing_handle )
+  if this.playing_handle ~= nil then
+    this.playing_handle:stop()
+  end
+--
+--   #EndIf
+--
+--   Return 1
+  return 1
+--
+-- End Function
+end
+
 -- Function __play_sound ( this As _char_type Ptr ) As Integer
 function __play_sound(this)
   log.debug("__play_sound called on "..this.id)
@@ -9,7 +26,8 @@ function __play_sound(this)
 --     .playing_handle = play_sample( llg( snd )[.sound[.chap]], .vol[.chap] )
   local vol = with0.vol[with0.chap]
   ll_global.snd[with0.sound[with0.chap]]:setVolume(vol and vol or 1)
-  with0.playing_handle = ll_global.snd[with0.sound[with0.chap]]:play()
+  with0.playing_handle = ll_global.snd[with0.sound[with0.chap]]
+  with0.playing_handle:play()
 --
 --   End With
 --
