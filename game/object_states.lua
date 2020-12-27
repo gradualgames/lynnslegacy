c342f9e6b86bc896017ad1e967aea00edf4e0c8f
Derek Andrews <gradualgames@gmail.com>
2020-12-27 18:58:48 -0500

    Implementing __cond_trigger_projectile.

--- game/object_states.lua
+++ game/object_states.lua
@@ -10,6 +10,29 @@ function __do_nothing(this)
 -- End Function
 end
 
+-- Function __cond_trigger_projectile ( this As _char_type Ptr ) As Integer
+function __cond_trigger_projectile(this)
+--
+--
+--
+--
+--   Dim As Integer per
+  local per = 0
+--
+--   per = Int ( Rnd * 10 )
+  per = math.floor(math.random() * 10)
+--
+--   If per >= 2 Then this->projectile->active = this->proj_style
+  if per >= 2 then this.projectile.active = this.proj_style end
+--
+--
+--   Return 1
+  return 1
+--
+--
+-- End Function
+end
+
 -- Function __infinity ( this As _char_type Ptr ) As Integer
 function __infinity(this)
 --
